51fb00eb26b951a9c7887967b80ed7ca39959c41
Ivan Torres <ivan@innetra.com>
2008-12-29 17:43:26 -0600

    removing pkg/

--- pkg/easy_role_authentication-0.0.1/generators/easy_role_authentication/templates/models/activity_log.rb
+++
@@ -1,2 +0,0 @@
-class ActivityLog < ActiveRecord::Base
-end
--- pkg/easy_role_authentication-0.0.1/init.rb
+++
@@ -1,4 +0,0 @@
-require File.dirname(__FILE__) + '/lib/easy_role_authentication'
-require File.dirname(__FILE__) + '/lib/by_password'
-require File.dirname(__FILE__) + '/lib/by_cookie_token'
-require File.dirname(__FILE__) + '/lib/user'
