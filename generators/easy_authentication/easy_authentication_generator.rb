3c4ceb30e22233e61ae699ba0ea0c41eb792d63b
Ivan Torres <ivan@innetra.com>
2009-01-09 16:06:36 -0600

    [new] generation of necessary routes (config/routes.rb)

--- generators/easy_authentication/easy_authentication_generator.rb
+++ generators/easy_authentication/easy_authentication_generator.rb
@@ -1,7 +1,8 @@
 require "digest/sha1"
 class EasyAuthenticationGenerator < Rails::Generator::Base
 
-  default_options :skip_layout => false, :skip_migrations => false
+  default_options :skip_layout => false, :skip_migrations => false,
+    :skip_routes => true
 
   def manifest
     record do |m|
@@ -64,6 +65,11 @@ class EasyAuthenticationGenerator < Rails::Generator::Base
       m.template "locales/es-MX.easy_authentication.yml",
         "config/locales/es-MX.easy_authentication.yml"
 
+      # Necessary Routes
+      unless options[:skip_routes]
+        generate_routes
+      end
+
       # Migrations
       unless options[:skip_migrations]
         m.migration_template "migrations/easy_authentication.rb", "db/migrate",
@@ -125,4 +131,36 @@ class EasyAuthenticationGenerator < Rails::Generator::Base
         "Don't map resources in routes file") { |v| options[:skip_routes] = v }
     end
 
+    def gsub_file(relative_destination, regexp, *args, &block)
+      path = destination_path(relative_destination)
+      content = File.read(path).gsub(regexp, *args, &block)
+      File.open(path, 'wb') { |file| file.write(content) }
+    end
+
+    def generate_routes
+      sentinel = 'ActionController::Routing::Routes.draw do |map|'
+
+      # Do not change indentation in this method!!!
+      gsub_file 'config/routes.rb', /(#{Regexp.escape(sentinel)})/mi do |match|
+        %{#{match}\n
+  # Easy Authentication
+  map.login "/login", :controller => "sessions", :action => "new"
+  map.logout "/logout", :controller => "sessions", :action => "destroy"
+  map.forgot_password "/forgot_password", :controller => "user_password",
+    :action => "forgot_password"
+  map.reset_password "/reset_password/:login/:token",
+    :controller => "user_password", :action => "reset_password", :method => "get"
+  map.reset_password "/reset_password",
+    :controller => "user_password", :action => "update_password", :method => "post"
+  map.change_password "/change_password", :controller => "user_password",
+    :action => "edit"
+
+  map.resources :users
+  map.resources :roles
+  map.resources :user_roles, :only => [:edit, :update]
+  map.resources :user_password, :only => [:edit, :update]
+  # Easy Authentication
+        }
+      end
+    end
 end
